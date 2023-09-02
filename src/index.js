f0606eac7aa441ec8cf8683c254cc318aff2f073
Louisa Goncharenko <louisa.gonch@gmail.com>
2023-09-02 15:43:05 -0400

    wrote tests for all methods in challenge 2

--- src/index.js
+++ src/index.js
@@ -1,53 +1,103 @@
-
 class CustomDate {
-    constructor(...args) {
-        this._date = new Date(...args)
-    }
+  constructor(...args) {
+    this._date = new Date(...args);
+  }
 
-    get date() {
-        return this._date.toLocaleDateString()
-    }
+  get dateNumber() {
+    return this._date.toLocaleDateString();
+  }
 
-    get string() {
-        return this._date.toString()
-    }
+  get string() {
+    return this._date.toString();
+  }
 
-    get year() { // getter starts with get
-		return this._date.getFullYear() // returns full year
-	}
+  get year() {
+    // getter starts with get
+    return this._date.getFullYear(); // returns full year
+  }
 
-    get yr() { // getter starts with get
-        return this._date.toLocaleDateString('en', {year: '2-digit'})
-	}
+  get yr() {
+    // getter starts with get
+    return this._date.toLocaleDateString("en", { year: "2-digit" });
+  }
 
-    get month() { // getter starts with get
-        const months = ['January','February','March','April','May','June', 'July','August','September','October','November','December']
-        return months[this._date.getMonth()]
-	}
+  get month() {
+    // getter starts with get
+    const months = [
+      "January",
+      "February",
+      "March",
+      "April",
+      "May",
+      "June",
+      "July",
+      "August",
+      "September",
+      "October",
+      "November",
+      "December",
+    ];
+    return months[this._date.getMonth()];
+  }
 
-    get mon() { // getter starts with get
-        const months = ['Jan','Feb','Mar','Apr','May','Jun', 'Jul','Aug','Sep','Oct','Nov','Dec']
-        return months[this._date.getMonth()]
-	}
+  get mon() {
+    // getter starts with get
+    const months = [
+      "Jan",
+      "Feb",
+      "Mar",
+      "Apr",
+      "May",
+      "Jun",
+      "Jul",
+      "Aug",
+      "Sep",
+      "Oct",
+      "Nov",
+      "Dec",
+    ];
+    return months[this._date.getMonth()];
+  }
 
-    get day() {
+  get day() {
+    const daysOfWeek = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+    const dayOfWeekNumber = this._date.getDay();
+    return daysOfWeek[dayOfWeekNumber];
+  }
 
-    }
+  get dy() {
+    const daysOfWeek = ['Sun', 'Mon', 'Tue', 'Wed', 'Thur', 'Fri', 'Sat'];
+    const dayOfWeekNumber = this._date.getDay();
+    return daysOfWeek[dayOfWeekNumber];
+  }
 
-    get dy() {
+  get date() {
+    return this._date.getDate()
+  }
 
-    }
+  get hour() {
+    return this._date.getHours()
+  }
+
+  get min() {
+    return this._date.getMinutes()
+  }
+
+  get secs() {
+    return this._date.getSeconds()
+  }
+  
 }
 
-const a = new CustomDate() // no parameters
-const b = new CustomDate('January 1, 1970') // with a string
-const c = new CustomDate(2001, 4, 12, 16, 45) // with year, month, date, hours, mins
-const d = new CustomDate(new Date()) // with another date object
-const e = new CustomDate('9/26/1965') 
-console.log(a.year, a.yr)
-console.log(b.year, b.yr)
-console.log(c.year, c.yr)
-console.log(d.year, d.yr)
-console.log(e.year, e.yr)
+const a = new CustomDate(); // no parameters
+const b = new CustomDate("January 1, 1970"); // with a string
+const c = new CustomDate(2001, 4, 12, 16, 45); // with year, month, date, hours, mins
+const d = new CustomDate(new Date()); // with another date object
+const e = new CustomDate("9/26/1965");
+console.log(a.date);
+console.log(b.day, b.dy);
+console.log(c.day, c.dy);
+console.log(d.day, d.dy);
+console.log(e.day, e.dy);
 
 module.exports.CustomDate = CustomDate;
--- tests/test.js
+++ tests/test.js
@@ -5,28 +5,30 @@ describe("CustomDate", () => {
   it("should initialize with the current date and time when no arguments are provided", () => {
     const currentDate = new Date();
     const customDate = new CustomDate();
-    expect(customDate.date).toBe(currentDate.toLocaleDateString());
+    expect(customDate.dateNumber).toBe(currentDate.toLocaleDateString());
   });
 
   // Test case for initializing with a date string
   it("should initialize with the provided date string", () => {
     const dateString = "January 1, 1970";
     const customDate = new CustomDate(dateString);
-    expect(customDate.date).toBe(new Date(dateString).toLocaleDateString());
+    expect(customDate.dateNumber).toBe(
+      new Date(dateString).toLocaleDateString()
+    );
   });
 
   // Test case for initializing with year, month, date, hours, mins
   it("should initialize with the provided year, month, date, hours, mins", () => {
     const newDate = new Date(2001, 4, 12, 16, 45);
     const customDate = new CustomDate(2001, 4, 12, 16, 45);
-    expect(customDate.date).toBe(newDate.toLocaleDateString());
+    expect(customDate.dateNumber).toBe(newDate.toLocaleDateString());
   });
 
   // Test case for initializing with another date object
   it("should initialize with another date object", () => {
     const newDate = new Date(new Date());
     const customDate = new CustomDate(new Date());
-    expect(customDate.date).toBe(newDate.toLocaleDateString());
+    expect(customDate.dateNumber).toBe(newDate.toLocaleDateString());
   });
 
   // Test case for getting the year with year, month, date
@@ -75,5 +77,65 @@ describe("CustomDate", () => {
   it("should return the correct abbreviated month when provide date string", () => {
     const customDate = new CustomDate("January 1, 1970");
     expect(customDate.mon).toBe("Jan");
+  });
+
+  // Test case for getting the day of the week
+  it("should return the correct day of the week", () => {
+    const customDate = new CustomDate(2023, 8, 2);
+    expect(customDate.day).toBe("Saturday");
+  });
+
+  // Test case for getting the day of the week
+  it("should return the correct day of the week when provide date string", () => {
+    const customDate = new CustomDate("January 1, 1970");
+    expect(customDate.day).toBe("Thursday");
+  });
+
+  // Test case for getting the abbreviated day of the week
+  it("should return the correct abbreviated day of the week", () => {
+    const customDate = new CustomDate(2023, 8, 2);
+    expect(customDate.dy).toBe("Sat");
+  });
+
+  // Test case for getting the abbreviatedday of the week
+  it("should return the correct abbreviated day of the week when provide date string", () => {
+    const customDate = new CustomDate("January 1, 1970");
+    expect(customDate.dy).toBe("Thur");
+  });
+
+  // Test case for getting the date
+  it("should return the correct date", () => {
+    const customDate = new CustomDate(2023, 8, 2);
+    expect(customDate.date).toBe(2);
+  });
+
+  // Test case for getting the date with date string
+  it("should return the correct date when provided date string", () => {
+    const customDate = new CustomDate("January 1, 1970");
+    expect(customDate.date).toBe(1);
+  });
+
+  // Test case for getting the date
+  it("should return the correct date", () => {
+    const customDate = new CustomDate(2023, 8, 2, 15, 30, 45);
+    expect(customDate.date).toBe(2);
+  });
+
+  // Test case for getting the hour
+  it("should return the correct hour", () => {
+    const customDate = new CustomDate(2023, 8, 2, 15, 30, 45);
+    expect(customDate.hour).toBe(15);
+  });
+
+  // Test case for getting the minutes
+  it("should return the correct minutes", () => {
+    const customDate = new CustomDate(2023, 8, 2, 15, 30, 45);
+    expect(customDate.min).toBe(30);
+  });
+
+  // Test case for getting the seconds
+  it("should return the correct seconds", () => {
+    const customDate = new CustomDate(2023, 8, 2, 15, 30, 45);
+    expect(customDate.secs).toBe(45);
   });
 });
